08edff9bcf469516bcc7bf4018ce4229df979704
Matthew Scheffel <matt@dataheck.com>
2024-06-17 21:09:10 -0300

    Correct error in personal note UUID selection
    this error would stop synchronization upon encountering an existing personal note

--- src/index.ts
+++ src/index.ts
@@ -194,218 +194,216 @@ function main () {
   
       loading = true
 
-      try {
-        const currentPage = await logseq.Editor.getCurrentPage()
-        if (currentPage?.originalName !== pageName) throw new Error('page error')
-  
-        const pageBlocksTree = await logseq.Editor.getCurrentPageBlocksTree()
+      const currentPage = await logseq.Editor.getCurrentPage()
+      if (currentPage?.originalName !== pageName) throw new Error('page error')
 
-        let targetBlock : BlockEntity | null = null;
-        let warningBlockFound = false;
-        for (const block of pageBlocksTree) {
-          if (block?.content.includes("LKRS")) {
-            targetBlock = block;
-            continue;
-          }
-          else if (block?.content.includes("BEGIN_WARNING")) {
-            warningBlockFound = true;
-          }
+      const pageBlocksTree = await logseq.Editor.getCurrentPageBlocksTree()
+
+      let targetBlock : BlockEntity | null = null;
+      let warningBlockFound = false;
+      for (const block of pageBlocksTree) {
+        if (block?.content.includes("LKRS")) {
+          targetBlock = block;
+          continue;
         }
-
-        if (!warningBlockFound) {
-          await logseq.Editor.insertBatchBlock(currentPage.uuid, [{ content: "\n#+BEGIN_WARNING\nPlease do not edit this page; stick to block references made elsewhere.\n#+END_WARNING" }], { sibling: false})
+        else if (block?.content.includes("BEGIN_WARNING")) {
+          warningBlockFound = true;
         }
+      }
 
-        const original_content = targetBlock?.content;
-        if (targetBlock === null || targetBlock === undefined) {
-          targetBlock = await logseq.Editor.insertBlock(currentPage.uuid, '🚀 LKRS: Please Select KOReader Metadata Directory ...',)
-        } else {
-          await logseq.Editor.updateBlock(targetBlock!.uuid, `🚀 LKRS: Please Select KOReader Metadata Directory ...`)
-        }
+      if (!warningBlockFound) {
+        await logseq.Editor.insertBatchBlock(currentPage.uuid, [{ content: "\n#+BEGIN_WARNING\nPlease do not edit this page; stick to block references made elsewhere.\n#+END_WARNING" }], { sibling: false})
+      }
 
-        let directoryHandle : any = await getStorage('logseq_koreader_sync__directoryHandle');
-        
-        let permission;
-        if (directoryHandle) {
-          permission = await verifyPermission(directoryHandle);
-        }
+      const original_content = targetBlock?.content;
+      if (targetBlock === null || targetBlock === undefined) {
+        targetBlock = await logseq.Editor.insertBlock(currentPage.uuid, '🚀 LKRS: Please Select KOReader Metadata Directory ...',)
+      } else {
+        await logseq.Editor.updateBlock(targetBlock!.uuid, `🚀 LKRS: Please Select KOReader Metadata Directory ...`)
+      }
 
-        if (!directoryHandle || !permission) {
-          try {
-            directoryHandle = await window.showDirectoryPicker() // get a DirectoryHandle that will allow us to read the contents of the directory
-          } catch (e) {
-            if (original_content) {
-              await logseq.Editor.updateBlock(targetBlock!.uuid, original_content)
-            } else {
-              await logseq.Editor.updateBlock(targetBlock!.uuid, "# ❌ LKRS: Sync cancelled by user.")
-            }
-            console.error(e);
-            return;
-          }
-          setStorage('logseq_koreader_sync__directoryHandle', directoryHandle);
-        }        
+      let directoryHandle : any = await getStorage('logseq_koreader_sync__directoryHandle');
+      
+      let permission;
+      if (directoryHandle) {
+        permission = await verifyPermission(directoryHandle);
+      }
 
-        if (!directoryHandle) {
-          console.error('No directory selected / found.')
-          return; // something went wrong
-        }
-
-        await logseq.Editor.updateBlock(targetBlock!.uuid, `# ⚙ LKRS: Processing KOReader Annotations ...`)
-
-        // FIXME: change the max value to the number of files in the directory
-        let fileCount = 0;
-        for await (const _ of walkDirectory(directoryHandle)) { fileCount++; };
-
-        // iterate over all blocks in this target page, and collect the titles, authors, and uuids and place them in a dictionary
-        let ret;
+      if (!directoryHandle || !permission) {
         try {
-          ret = await logseq.DB.datascriptQuery(`
-          [
-              :find (pull ?b [:block/content :block/uuid]) ?authors
-              :where
-                [?b :block/parent ?p]
-                [?p :block/uuid #uuid "${targetBlock!.uuid}"]
-                [?b :block/properties ?props]
-                [(get ?props :authors) ?authors]
-          ]
-          `)
+          directoryHandle = await window.showDirectoryPicker() // get a DirectoryHandle that will allow us to read the contents of the directory
         } catch (e) {
-          console.error("Error while iterating over blocks in the target page: ", e);
+          if (original_content) {
+            await logseq.Editor.updateBlock(targetBlock!.uuid, original_content)
+          } else {
+            await logseq.Editor.updateBlock(targetBlock!.uuid, "# ❌ LKRS: Sync cancelled by user.")
+          }
+          console.error(e);
           return;
         }
+        setStorage('logseq_koreader_sync__directoryHandle', directoryHandle);
+      }        
 
-        const titleMatch : RegExp = /##\s+(.*?)\n/;
+      if (!directoryHandle) {
+        console.error('No directory selected / found.')
+        return; // something went wrong
+      }
 
-        let existingBlocks = {}
-        for (const block of ret) {
-          const authors = block[1];
-          const content = block[0]["content"];
-          const match = content?.match(titleMatch);
-          let title = match[1];
+      await logseq.Editor.updateBlock(targetBlock!.uuid, `# ⚙ LKRS: Processing KOReader Annotations ...`)
 
-          const key = authors + "___" +  title;
-          if (!(key in existingBlocks)) {
-            existingBlocks[key] = block[0]["uuid"];
+      // FIXME: change the max value to the number of files in the directory
+      let fileCount = 0;
+      for await (const _ of walkDirectory(directoryHandle)) { fileCount++; };
+
+      // iterate over all blocks in this target page, and collect the titles, authors, and uuids and place them in a dictionary
+      let ret;
+      try {
+        ret = await logseq.DB.datascriptQuery(`
+        [
+            :find (pull ?b [:block/content :block/uuid]) ?authors
+            :where
+              [?b :block/parent ?p]
+              [?p :block/uuid #uuid "${targetBlock!.uuid}"]
+              [?b :block/properties ?props]
+              [(get ?props :authors) ?authors]
+        ]
+        `)
+      } catch (e) {
+        console.error("Error while iterating over blocks in the target page: ", e);
+        return;
+      }
+
+      const titleMatch : RegExp = /##\s+(.*?)\n/;
+
+      let existingBlocks = {}
+      for (const block of ret) {
+        const authors = block[1];
+        const content = block[0]["content"];
+        const match = content?.match(titleMatch);
+        let title = match[1];
+
+        const key = authors + "___" +  title;
+        if (!(key in existingBlocks)) {
+          let block_uuid = block[0]["uuid"];
+          if (block_uuid) {
+            existingBlocks[key] = block_uuid;
           }
         }
+      }
 
-        const syncProgress = new ProgressNotification("Syncing Koreader Annotations to Logseq:", fileCount);
-        for await (const fileHandle of walkDirectory(directoryHandle)) {
-          var text = await fileHandle.text();
-          var parsed_block = lua_to_block(text);
+      const syncProgress = new ProgressNotification("Syncing Koreader Annotations to Logseq:", fileCount);
+      for await (const fileHandle of walkDirectory(directoryHandle)) {
+        var text = await fileHandle.text();
+        var parsed_block = lua_to_block(text);
 
-          if (parsed_block) {
-            let key: string;
-            if (parsed_block.properties!.authors === undefined) {
-              key = "___" + parsed_block.content.substring(3);
-            } else {
-              key = parsed_block.properties!.authors + "___" + parsed_block.content.substring(3);
+        if (parsed_block) {
+          let key: string;
+          if (parsed_block.properties!.authors === undefined) {
+            key = "___" + parsed_block.content.substring(3);
+          } else {
+            key = parsed_block.properties!.authors + "___" + parsed_block.content.substring(3);
+          }
+
+          // Has this been synced before?
+          if (key in existingBlocks) {             
+            const existing_block = await logseq.Editor.getBlock(existingBlocks[key]);
+            if (existing_block === null) {
+              console.error("Block not found, but we also just found it - which is pretty weird: ", existingBlocks[key]);
+              continue;
             }
 
-            // Has this been synced before?
-            if (key in existingBlocks) {             
-              const existing_block = await logseq.Editor.getBlock(existingBlocks[key]);
-              if (existing_block === null) {
-                console.error("Block not found, but we also just found it - which is pretty weird: ", existingBlocks[key]);
-                continue;
+            // find the bookmarks block
+            let existing_bookmark_blocks;
+            let existing_bookmark_block_uuid;
+
+            for (const child of existing_block!.children!) {
+              let child_block = await logseq.Editor.getBlock(child[1] as BlockEntity);
+
+              if (child_block!.content === "### Bookmarks") {
+                existing_bookmark_blocks = child_block!.children;
+                existing_bookmark_block_uuid = child[1];
+
+                break;
+              }
+            }
+
+            if (existing_bookmark_blocks === undefined) {
+              console.error("Bookmarks not found for block ", existingBlocks[key]);
+              continue;
+            }
+
+            // iterate over bookmarks and build a dictionary for easy lookup
+            let existing_bookmarks = {};
+            for (const bookmark of existing_bookmark_blocks) {
+              let bookmark_block = await logseq.Editor.getBlock(bookmark[1] as BlockEntity);
+
+              const content_start = bookmark_block!.content!.indexOf("\n> ");     // not ideal
+              const content = bookmark_block!.content!.substring(content_start+3).replace('-', '\-');
+
+              existing_bookmarks[content] = bookmark[1];
+            }
+
+            // iterate over bookmarks in `block`, checking if they already exist
+            // the first child of `parsed_block` is the "### Bookmarks" block
+            for (const bookmark of parsed_block.children![0].children!) {
+              let key = bookmark.content.substring(2);
+
+              // does this parsed block have a personal note?
+              let parsed_personal_note = false;
+              if (bookmark.children && bookmark.children.length > 0) {
+                parsed_personal_note = true;
               }
 
-              // find the bookmarks block
-              let existing_bookmark_blocks;
-              let existing_bookmark_block_uuid;
+              // existing bookmark, check personal note
+              if (key in existing_bookmarks) {
+                let existing_bookmark = await logseq.Editor.getBlock(existing_bookmarks[key]);
+                
+                // personal note exists in graph
+                if (existing_bookmark!.children && existing_bookmark!.children!.length > 0) {
+                  let existing_note = existing_bookmark!.children![0];
 
-              for (const child of existing_block!.children!) {
-                let child_block = await logseq.Editor.getBlock(child[1] as BlockEntity);
+                  if (!parsed_personal_note) {
+                    // delete it
+                    await logseq.Editor.removeBlock(existing_note[1] as BlockUUID);
+                  } else {
+                    let existing_note_block = await logseq.Editor.getBlock(existing_note[1] as BlockEntity);
 
-                if (child_block!.content === "### Bookmarks") {
-                  existing_bookmark_blocks = child_block!.children;
-                  existing_bookmark_block_uuid = child[1];
-                  break;
-                }
-              }
-
-              if (existing_bookmark_blocks === undefined) {
-                console.error("Bookmarks not found for block ", existingBlocks[key]);
-                continue;
-              }
-
-              // iterate over bookmarks and build a dictionary for easy lookup
-              let existing_bookmarks = {};
-              for (const bookmark of existing_bookmark_blocks) {
-                let bookmark_block = await logseq.Editor.getBlock(bookmark[1] as BlockEntity);
-
-                const content_start = bookmark_block!.content!.indexOf("\n> ");     // not ideal
-                const content = bookmark_block!.content!.substring(content_start+3).replace('-', '\-');
-
-                existing_bookmarks[content] = bookmark[1];
-              }
-
-              // iterate over bookmarks in `block`, checking if they already exist
-              // the first child of `parsed_block` is the "### Bookmarks" block
-              for (const bookmark of parsed_block.children![0].children!) {
-                let key = bookmark.content.substring(2);
-
-                // does this parsed block have a personal note?
-                let parsed_personal_note = false;
-                if (bookmark.children && bookmark.children.length > 0) {
-                  parsed_personal_note = true;
-                }
-
-                // existing bookmark, check personal note
-                if (key in existing_bookmarks) {
-                  let existing_bookmark = await logseq.Editor.getBlock(existing_bookmarks[key]);
-                  
-                  // personal note exists in graph
-                  if (existing_bookmark!.children && existing_bookmark!.children!.length > 0) {
-                    let existing_note = existing_bookmark!.children![0];
-
-                    if (!parsed_personal_note) {
-                      // delete it
-                      await logseq.Editor.removeBlock(existing_note[1] as BlockUUID);
-                    } else {
-                      let existing_note_block = await logseq.Editor.getBlock(existing_note[1] as BlockEntity);
-
-                      // if the existing note is different, update it
-                      if (existing_note_block!.content !== bookmark.children![0].content) {
-                        await logseq.Editor.updateBlock(existing_note[1] as string, bookmark.children![0].content);
-                      }
-                    }
-                  } 
-                  // personal note does not exist in graph
-                  else {
-                    // add it
-                    if (parsed_personal_note) {
-                      await logseq.Editor.insertBatchBlock(existing_bookmark![1] as string, [bookmark.children![0]], {
-                        sibling: false
-                      })
+                    // if the existing note is different, update it
+                    if (existing_note_block!.content !== bookmark.children![0].content) {
+                      await logseq.Editor.updateBlock(existing_note[1] as string, bookmark.children![0].content);
                     }
                   }
                 } 
-                // new bookmark, add it
+                // personal note does not exist in graph
                 else {
-                  await logseq.Editor.insertBatchBlock(existing_bookmark_block_uuid, [bookmark], {
-                    sibling: false
-                  })
+                  // add it
+                  if (parsed_personal_note) {
+                    await logseq.Editor.insertBatchBlock(existing_bookmark!.uuid, [bookmark.children![0]], {
+                      sibling: false
+                    })
+                  }
                 }
+              } 
+              // new bookmark, add it
+              else {
+                await logseq.Editor.insertBatchBlock(existing_bookmark_block_uuid, [bookmark], {
+                  sibling: false
+                })
               }
-            } else {
-              await logseq.Editor.insertBatchBlock(targetBlock!.uuid, [parsed_block], {
-                sibling: false
-              })
             }
+          } else {
+            await logseq.Editor.insertBatchBlock(targetBlock!.uuid, [parsed_block], {
+              sibling: false
+            })
           }
-          syncProgress.increment(1);
         }
-
-        await logseq.Editor.updateBlock(targetBlock!.uuid, `# 📚 LKRS: KOReader - Sync Initiated at ${syncTimeLabel}`)
-        
-        syncProgress.destruct();
-      } catch (e) {
-        logseq.UI.showMsg(e.toString(), 'warning')
-        console.error(e)
-      } finally {
-        loading = false
+        syncProgress.increment(1);
       }
+
+      await logseq.Editor.updateBlock(targetBlock!.uuid, `# 📚 LKRS: KOReader - Sync Initiated at ${syncTimeLabel}`)
+      
+      syncProgress.destruct();
+      loading = false
     }
   })
 
